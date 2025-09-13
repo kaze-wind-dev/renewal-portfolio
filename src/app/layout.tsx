94bf61e7e05002eb6c61e49648a447aa76f845c8
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-09-13 22:26:52 +0900

    style: next/fontの設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,33 @@
+import clsx from "clsx";
+import { Zen_Kaku_Gothic_New, Outfit } from "next/font/google";
+import "@/styles/global.scss";
+
+const ZenKakuGothicNewFont = Zen_Kaku_Gothic_New({
+  weight: ["500", "700"],
+  variable: "--font-zenkaku-gothic-new",
+  display: "swap",
+  preload: true,
+});
+
+const OutfitFont = Outfit({
+  weight: "700",
+  subsets: ["latin"],
+  variable: "--font-outfit",
+  display: "swap",
+  preload: false,
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
-      <body className={``}>
-        {children}
-      </body>
+    <html
+      lang="ja"
+      className={clsx(ZenKakuGothicNewFont.variable, OutfitFont.variable)}
+    >
+      <body>{children}</body>
     </html>
   );
 }
