378038934c9ad8c6f161f52469413e9b36200fb0
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-09-14 20:27:49 +0900

    feature: topHeroの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import clsx from "clsx";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <section className={clsx(styles["p-topHero"])}>
+      <div className={clsx(styles["p-topHero__inner"], "inner")}>
+        <p className={clsx(styles["p-topHero__en-text"])}>
+          From coder to <br />
+          Front-end engineer.
+        </p>
+        <h1 className={clsx(styles["p-topHero__heading"])}>
+          コーダーからフロントエンドエンジニアへ。
+          <br />
+          3年間の経験を活かし、
+          <br />
+          次のステージを目指してしています。
+        </h1>
+        <div
+          className={clsx(styles["p-topHero__scroll-guide"])}
+          aria-hidden="true"
+        >
+          <span
+            className={clsx(styles["p-topHero__scroll-guide__text"])}
+            aria-hidden="true"
+          >
+            scroll
+          </span>
+          <div
+            className={clsx(styles["p-topHero__scroll-guide__bar"])}
+            aria-hidden="true"
+          ></div>
+        </div>
+      </div>
+    </section>
   );
 }
