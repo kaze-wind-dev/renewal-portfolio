d39282e390084794d21c74bd1b534999a8b4098d
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-09-16 23:49:07 +0900

    feature: topArticlesの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,54 @@
 import clsx from "clsx";
-import { WORKS_LIST_LIMIT } from "@/constants";
+import { WORKS_LIST_LIMIT, TOP_ZENN_LIST_LIMIT } from "@/constants";
 import TopWorks from "@/components/sections/topWorks";
+import TopArticles from "@/components/sections/TopArticles";
 import { getWorksList } from "@/lib/api/microcms";
+import { getZennArticles } from "@/lib/api/zennFunctions";
 import styles from "./page.module.scss";
 
 export default async function Home() {
-    const { contents: works } = await getWorksList({
+  const { contents: works } = await getWorksList({
     limit: WORKS_LIST_LIMIT,
   });
+  const zennArticlesData = await getZennArticles({
+    count: TOP_ZENN_LIST_LIMIT,
+    order: "latest",
+  });
+  console.log(zennArticlesData);
   return (
     <>
-    <section className={clsx(styles["p-topHero"])}>
-      <div className={clsx(styles["p-topHero__inner"], "inner")}>
-        <p className={clsx(styles["p-topHero__en-text"])}>
-          From coder to <br />
-          Front-end engineer.
-        </p>
-        <h1 className={clsx(styles["p-topHero__heading"])}>
-          コーダーからフロントエンドエンジニアへ。
-          <br />
-          3年間の経験を活かし、
-          <br />
-          次のステージを目指してしています。
-        </h1>
-        <div
-          className={clsx(styles["p-topHero__scroll-guide"])}
-          aria-hidden="true"
-        >
-          <span
-            className={clsx(styles["p-topHero__scroll-guide__text"])}
+      <section className={clsx(styles["p-topHero"])}>
+        <div className={clsx(styles["p-topHero__inner"], "inner")}>
+          <p className={clsx(styles["p-topHero__en-text"])}>
+            From coder to <br />
+            Front-end engineer.
+          </p>
+          <h1 className={clsx(styles["p-topHero__heading"])}>
+            コーダーからフロントエンドエンジニアへ。
+            <br />
+            3年間の経験を活かし、
+            <br />
+            次のステージを目指してしています。
+          </h1>
+          <div
+            className={clsx(styles["p-topHero__scroll-guide"])}
             aria-hidden="true"
           >
-            scroll
-          </span>
-          <div
-            className={clsx(styles["p-topHero__scroll-guide__bar"])}
-            aria-hidden="true"
-          ></div>
+            <span
+              className={clsx(styles["p-topHero__scroll-guide__text"])}
+              aria-hidden="true"
+            >
+              scroll
+            </span>
+            <div
+              className={clsx(styles["p-topHero__scroll-guide__bar"])}
+              aria-hidden="true"
+            ></div>
+          </div>
         </div>
-      </div>
-    </section>
-    <TopWorks works={works} />
+      </section>
+      <TopWorks works={works} />
+      <TopArticles articles={zennArticlesData.articles} />
     </>
   );
 }
---
+++ src/components/sections/TopArticles/index.tsx
@@ -0,0 +1,119 @@
+"use client";
+import Link from "next/link";
+import clsx from "clsx";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { useRef, useEffect } from "react";
+import { Splide, SplideTrack, SplideSlide } from "@splidejs/react-splide";
+import SectionTitle from "@/components/ui/SectionTitle";
+import ArticleCard from "@/components/ui/Card/ArticleCard";
+import CustomSplideArrows from "@/components/ui/CustomSprideArrows";
+import type { ZennArticle } from "@/types/zenn";
+import styles from "./index.module.scss";
+
+type Props = {
+  articles: ZennArticle[];
+};
+const TopArticles = ({ articles }: Props) => {
+  const splideRef = useRef(null);
+  const splideOptions = {
+    type: "loop",
+    focus: "left",
+    gap: "1.875rem",
+    arrows: false,
+    pagination: false,
+    fixedWidth: "300px",
+    breakpoints: {
+      960: {
+        gap: "1.5rem",
+
+        fixedWidth: "280px",
+      },
+      560: {
+        fixedWidth: "64%",
+      },
+      480: {
+        gap: "1rem",
+        fixedWidth: "72%",
+      },
+      400: {
+        fixedWidth: "80%",
+      },
+    },
+  };
+ useEffect(() => {
+    if (!splideRef.current) return;
+    const splide = splideRef.current.splide;
+    if (!splide) return;
+    const bar = splide.root.querySelector(".topArticles-progress");
+    const updateProgress = () => {
+      const end = splide.Components.Controller.getEnd() + 1;
+      const rate = Math.min((splide.index + 1) / end, 1);
+      bar.style.width = String(100 * rate) + "%";
+    };
+    splide.on("mounted move", updateProgress);
+    return () => {
+      splide.off("mounted move", updateProgress);
+    };
+  }, []);
+
+  return (
+    <section className={clsx(styles["p-topArticles"])}>
+      <div className={clsx(styles["p-topArticles__inner"])}>
+        <SectionTitle
+          className={clsx(styles["p-topArticles__title"])}
+          heading={<>Articles</>}
+          text={<>投稿記事</>}
+          position="left"
+        />
+
+        <Splide
+          options={splideOptions}
+          aria-label="Zenn記事の一覧"
+          tag="section"
+          className={clsx(styles["p-topArticles__slider"])}
+          ref={splideRef}
+          hasTrack={false}
+        >
+          <SplideTrack className={clsx(styles["p-topArticles__slider-track"])}>
+            {articles.map((article) => {
+              return (
+                <SplideSlide
+                  className={clsx(styles["p-topArticles__slider-slide"])}
+                  key={article.id}
+                >
+                  <ArticleCard article={article} key={article.id} />
+                </SplideSlide>
+              );
+            })}
+          </SplideTrack>
+
+           <div className={clsx(styles["p-topArticles__actions"])}>
+            <div className={clsx(styles["p-topArticles__controls"])}>
+              <CustomSplideArrows
+                splideRef={splideRef}
+                className={clsx(styles["p-topArticles__slider-arrows"])}
+              />
+              <div className={clsx(styles["p-topArticles__progress"])}>
+                <div
+                  className={clsx(
+                    styles["p-topArticles__progress-bar"],
+                    "topArticles-progress"
+                  )}
+                ></div>
+              </div>
+            </div>
+            <Link href="/articles" className={styles["p-topArticles__moreLink"]}>
+              一覧を見る
+              <HiOutlineArrowNarrowRight
+                className={clsx(styles["p-topArticles__moreLink-arrow"])}
+              />
+            </Link>
+          </div>
+        </Splide>
+
+      </div>
+    </section>
+  );
+};
+
+export default TopArticles;
