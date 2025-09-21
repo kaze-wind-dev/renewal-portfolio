79d3606a070c710cc01e349cde454f92cee078bb
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-09-21 21:30:21 +0900

    featuer: categoryFilterの追加

---
+++ src/components/ui/CategoryFilter/index.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Link from "next/link";
+import { Category } from "@/types/microcms";
+import styles from "./index.module.scss";
+
+type CategoryFilterProps = {
+  categories: Category[];
+  basePath?: string;
+  currentId?: string;
+};
+
+const CategoryFilter = ({
+  categories,
+  basePath = "works",
+  currentId,
+}: CategoryFilterProps) => {
+
+  return (
+    <ul className={`${styles["c-categoryFilter"]}`}>
+      {currentId ? (
+        <li className={`${styles["c-categoryFilter__btn"]}`}>
+          <Link
+            className={`${styles["c-categoryFilter__link"]}`}
+            href={`/${basePath}`}
+          >
+            ALL
+          </Link>
+        </li>
+      ) : (
+        <li className={`${styles["c-categoryFilter__btn"]}`}>
+          <span
+            className={`${styles["c-categoryFilter__link"]} ${styles["c-categoryFilter__link--current"]}`}
+          >
+            ALL
+          </span>
+        </li>
+      )}
+      {categories.map((category) =>
+        currentId === category.id ? (
+          <li
+            key={category.id}
+            className={`${styles["c-categoryFilter__btn"]}`}
+          >
+            <span
+              className={`${styles["c-categoryFilter__link"]} ${styles["c-categoryFilter__link--current"]}`}
+            >
+              {category.name}
+            </span>
+          </li>
+        ) : (
+          <li
+            key={category.id}
+            className={`${styles["c-categoryFilter__btn"]}`}
+          >
+            <Link
+              className={`${styles["c-categoryFilter__link"]}`}
+              href={`/${basePath}/category/${category.id}`}
+            >
+              {category.name}
+            </Link>
+          </li>
+        )
+      )}
+    </ul>
+  );
+};
+
+export default CategoryFilter;
