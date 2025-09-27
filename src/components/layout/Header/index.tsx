8100600137fe324f09dd099ea691a7f612c9c092
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-09-27 14:15:51 +0900

    refactor: headerナビゲーションをNavigationを使用した記述に変更

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,43 +1,27 @@
-import Link from "next/link";
+import { MAIN_NAVIGATION_LIST } from "@/constants";
 import Logo from "@/components/ui/Logo";
 import IconList from "@/components/ui/IconList";
 import MobileMenu from "@/components/ui/MobileMenu";
+import { Navigation } from "@/components/ui/Navigation";
 
 const Header = () => {
+  const headerNavClassName = {
+    nav: "l-header__nav",
+    list: "l-header__nav-list",
+    item: "l-header__nav-item",
+    link: "l-header__nav-link",
+    linkCurrent: "l-header__nav-link--current",
+  };
   return (
     <header className="l-header" id="header">
       <div className="l-header__inner">
-        <Logo className="l-header__logo" link={true}/>
+        <Logo className="l-header__logo" link={true} />
         <div className="l-header__line">
-          <nav className="l-header__nav" aria-label="ヘッダーナビゲーション">
-            <ul className="l-header__nav-list">
-              <li className="l-header__nav-item">
-                <Link href="/" className="l-header__nav-link">
-                  Top
-                </Link>
-              </li>
-              <li className="l-header__nav-item">
-                <Link href="/works" className="l-header__nav-link">
-                  Works
-                </Link>
-              </li>
-              <li className="l-header__nav-item">
-                <Link href="/articles" className="l-header__nav-link">
-                  Articles
-                </Link>
-              </li>
-              <li className="l-header__nav-item">
-                <Link href="/about" className="l-header__nav-link">
-                  About
-                </Link>
-              </li>
-              <li className="l-header__nav-item">
-                <Link href="/contact" className="l-header__nav-link">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <Navigation
+            items={MAIN_NAVIGATION_LIST}
+            className={headerNavClassName}
+            ariaLabel="ヘッダーナビゲーション"
+          />
           <div className="l-header__icon-list">
             <IconList />
           </div>
