0c1c1ffb7b07bc09ff8b10c1a9cc0b8b95d370de
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-10-03 11:27:43 +0900

    fix: worksの詳細ページでもカレント表示がされるようにisCurrent関数を修正

--- src/lib/utils/navigation.ts
+++ src/lib/utils/navigation.ts
@@ -1,3 +1,7 @@
 export function isCurrent(pathname: string, hrefPath: string) {
+  if (hrefPath === "/") {
     return pathname === hrefPath;
+  } else {
+    return pathname.includes(hrefPath);
+  }
 }
